48fb580f9cf20489eaed8e893e0e4fab7517d875
lukefromdc <lukefromdc@hushmail.com>
2023-11-29 16:51:54 -0500

    auto-restart core session components if crashed
    *Same behavior as on x11: restart mate-panel, caja, the notification daemon, and polkit agent if stopped or crashed
    *We have to close the subshells on exit to stop subshell processes from continuing after logout

--- data/mate-wayland-components.sh
+++ data/mate-wayland-components.sh
@@ -5,22 +5,23 @@ hash dbus-update-activation-environment 2>/dev/null && dbus-update-activation-en
 
 #Programs to restart while compositor is running, matching Xorg behavior
 
-#(while $pgrep wayfire !=""; do
-mate-panel &
-#done) &
 
-#(while $pgrep wayfire !=""; do
-/usr/libexec/polkit-mate-authentication-agent-1 &
-#done) &
+(while true; do
+mate-panel
+done) &
 
-#(while $pgrep wayfire !=""; do
-/usr/libexec/mate-notification-daemon &
-#done) &
+(while true; do
+/usr/libexec/polkit-mate-authentication-agent-1
+done) &
 
-#(while $pgrep wayfire !=""; do
-caja -n --force-desktop &
-#done) &
+(while true; do
+/usr/libexec/mate-notification-daemon
+done) &
+
+(while true; do
+caja -n --force-desktop
+done) &
 
 #Programs to start once, matching Xorg behavior
-nm-applet --indicator
+nm-applet --indicator &
 
--- data/mate-wayland.sh
+++ data/mate-wayland.sh
@@ -1,1 +1,5 @@
+#Start the compositor using a mate-specific wayfire.ini
 wayfire -c /etc/mate/wayfire.ini
+
+#Clean up subshells on exit
+killall sh
