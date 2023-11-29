8a6ab268305666de9b4d73433e41d8f30096cdbe
lukefromdc <lukefromdc@hushmail.com>
2023-11-29 15:34:49 -0500

    Move MATE components out of wayfire.ini
    Move the MATE components to a separate file

---
+++ data/mate-wayland-components.sh
@@ -0,0 +1,21 @@
+#Programs to restart while compositor is running, matching Xorg behavior
+
+#(while $pgrep wayfire !=""; do
+mate-panel &
+#done) &
+
+#(while $pgrep wayfire !=""; do
+/usr/libexec/polkit-mate-authentication-agent-1 &
+#done) &
+
+#(while $pgrep wayfire !=""; do
+/usr/libexec/mate-notification-daemon &
+#done) &
+
+#(while $pgrep wayfire !=""; do
+caja -n --force-desktop &
+#done) &
+
+#Programs to start once, matching Xorg behavior
+nm-applet --indicator
+
