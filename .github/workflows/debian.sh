df184699eaefb8826659490ddbaab8cc5d3262eb
mbkma <johannes.unruh@fau.de>
2026-01-27 23:12:51 +0100

    ci: migrate from Travis CI to GitHub Actions
    Replace Travis CI configuration with GitHub Actions workflows.
    Add CI build workflow for Debian, Fedora, Ubuntu, and Archlinux.
    Add release workflow for automated GitHub releases on tags.
    Add dependabot configuration for GHA pin updates.
    Remove obsolete .travis.yml.

---
+++ .github/workflows/debian.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/bash
+
+# Use grouped output messages
+infobegin() {
+	echo "::group::${1}"
+}
+infoend() {
+	echo "::endgroup::"
+}
+
+# Required packages on Debian
+requires=(
+	ccache # Use ccache to speed up build
+	meson  # Used for meson build
+)
+
+requires+=(
+	autopoint
+	gcc
+	git
+	libxml2-dev
+	make
+	mate-common
+	yelp-tools
+)
+
+infobegin "Update system"
+apt-get update -qq
+infoend
+
+infobegin "Install dependency packages"
+env DEBIAN_FRONTEND=noninteractive \
+	apt-get install --assume-yes \
+	${requires[@]}
+infoend
